3bc8b9ca5cc612f16db1a6b4d120aefe6abd4766
SevcanDogramaci <sevcan.dog@gmail.com>
2020-06-20 14:37:08 +0300

    Test update

--- tests/test2.s
+++ tests/test2.s
@@ -1,11 +1,19 @@
 jal label
+
+slt $r1, $r0, $sp
+slti $r2, $r0, 300
+
+sw $r1, 0($sp)
+lw $r0, 0($sp)
+sll $r1, $r1, $r1
+srl $r2, $r1, $r1
+lui $r3, 2
+muli $r1, $r1, 5
+mul $r1, $r1, $r2
+bne $r0, $r1, exit
+
 label: sub $sp, $sp, $r0
 jr $ra
-sw $r0, 0($sp)
-lw $r1, 0($sp)
-sll $r1, $r1, $r1
-srl $r1, $r1, $r1
-lui $r3, 2
-muli $r1, $r1, 0
-mul $r1, $r1, $r2
-bne $r0, $r1, label
+
+exit:
+syscall $sp
